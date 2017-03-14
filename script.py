24caaa98cd784ea01270f8537e3ec7aa9f970d2b
catchwhale <mrjoe8888@gmail.com>
2017-03-14 22:11:05 +0800

    Please enter the commit message for your changes. Lines starting

--- script.py
+++ script.py
@@ -28,41 +28,8 @@ touch /etc/squid3/squid_passwd
 chown proxy /etc/squid3/squid_passwd
 initctl show-config squid3 
 """
-append_conf="""
-forwarded_for off
-request_header_access Allow allow all
-request_header_access Authorization allow all
-request_header_access WWW-Authenticate allow all
-request_header_access Proxy-Authorization allow all
-request_header_access Proxy-Authenticate allow all
-request_header_access Cache-Control allow all
-request_header_access Content-Encoding allow all
-request_header_access Content-Length allow all
-request_header_access Content-Type allow all
-request_header_access Date allow all
-request_header_access Expires allow all
-request_header_access Host allow all
-request_header_access If-Modified-Since allow all
-request_header_access Last-Modified allow all
-request_header_access Location allow all
-request_header_access Pragma allow all
-request_header_access Accept allow all
-request_header_access Accept-Charset allow all
-request_header_access Accept-Encoding allow all
-request_header_access Accept-Language allow all
-request_header_access Content-Language allow all
-request_header_access Mime-Version allow all
-request_header_access Retry-After allow all
-request_header_access Title allow all
-request_header_access Connection allow all
-request_header_access Proxy-Connection allow all
-request_header_access User-Agent allow all
-request_header_access Cookie allow all
-request_header_access All deny all
-auth_param basic program /usr/lib/squid3/basic_ncsa_auth /etc/squid3/squid_passwd
-acl ncsa_users proxy_auth REQUIRED
-http_access allow ncsa_users
-"""
+append_conf = os.popen("cat 'Ubuntu Suid Setup Proxy.txt'").readlines()
+
 user = os.popen('users').read().strip(); #get the current user
 os.chdir(dir_); # cd directory
 list_of_users = []; # initializing list
